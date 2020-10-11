3489312215b46edbbabc6ca30b27027c138ac728
David Hunter Walsh <davidhunterwalsh@gmail.com>
2020-10-11 15:18:35 -0400

    Added the readme, and the markdown step to produce it

--- plan.R
+++ plan.R
@@ -1,9 +1,16 @@
 the_plan <- drake_plan(
 
-  data      = read_sheet('1Eo23GqsjOYIW0Y2bKzJBcmcPj0iXVqIsEzfDjPAALa8'),
+  data        = read_sheet('1Eo23GqsjOYIW0Y2bKzJBcmcPj0iXVqIsEzfDjPAALa8'),
   
-  plot      = make_weights_plot(data),
+  plot        = make_weights_plot(data),
   
-  plot_jpeg = ggsave(file_out('img/Kitten_Weights.jpeg'), plot, jpeg())
-
+  plot_jpeg   = ggsave(file_out('img/Kitten_Weights.jpeg'), plot, jpeg()),
+  
+  make_readme = render(
+      input         = knitr_in("doc/readme.Rmd"),
+      output_format = 'md_document',
+      output_file   = file_out("readme.md"),
+      output_dir    = '.'
+    )
+  
 )
---
+++ readme.md
@@ -0,0 +1,6 @@
+Kitten Science.
+===============
+
+Serious business.
+
+![](C:/Users/DHW/Documents/kittenscience/readme_files/figure-markdown_strict/weights_plot-1.png)
